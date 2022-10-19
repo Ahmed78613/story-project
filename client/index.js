e4571721ce4a37159cd2512c92eb99ad647a0c58
ahmed <ahmedmuhammed78613@gmail.com>
2022-10-19 11:52:42 +0100

    updated css and js

--- client/index.js
+++ client/index.js
@@ -1,17 +1,19 @@
 // Forms
-const loginForm = document.getElementById(".login-form");
-const registerForm = document.getElementById(".register-form");
+const loginForm = document.getElementById("login-form");
+const registerForm = document.getElementById("register-form");
 // Form buttons
 const loginBtn = document.getElementById("login-btn");
 const registerBtn = document.getElementById("register-btn");
 // Containers
 const loginContainer = document.getElementById("login-container");
 const registerContainer = document.getElementById("register-container");
-
-// Register & Login Buttons
+// Event Listeners
 loginBtn.addEventListener("click", switchForms);
 registerBtn.addEventListener("click", switchToRegister);
+loginForm.addEventListener("submit", handleLogin);
+registerForm.addEventListener("submit", handleRegister);
 
+// Switch Forms
 function switchToRegister() {
 	loginContainer.style.display = "none";
 	registerContainer.style.display = "flex";
@@ -23,17 +25,20 @@ function switchForms() {
 }
 
 // Login Form submit
-loginForm.addEventListener("submit", handleLogin);
-
 function handleLogin(e) {
 	e.preventDefault();
-	console.log(e.target.username.value, e.target.password.value);
+	const username = e.target.username.value;
+	const password = e.target.password.value;
 }
 
-// Login Form submit
-registerForm.addEventListener("submit", handleRegister);
-
+// Register Form submit
 function handleRegister(e) {
 	e.preventDefault();
-	console.log(e.target.username.value, e.target.password.value);
+	const username = e.target.registerUsername.value;
+	const password = e.target.registerPassword.value;
+	const confirmPassword = e.target.confirmPassword.value;
+	// Check if password math
+	password === confirmPassword
+		? console.log("yes")
+		: alert("Passwords don't match. Please try again.");
 }
